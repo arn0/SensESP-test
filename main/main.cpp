a98da3428df683bbba9d7c199348e9bf08e068c0
arn0 <arnoc@xs4all.nl>
2024-04-03 18:47:22 +0200

    Start working main()

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,44 @@
-#include <stdio.h>
+// Signal K application template file.
+//
+// This application demonstrates core SensESP concepts in a very
+// concise manner. You can build and upload the application as is
+// and observe the value changes on the serial port monitor.
+//
+// You can use this source file as a basis for your own projects.
+// Remove the parts that are not relevant to you, and add your own code
+// for external hardware libraries.
 
-extern "C" void app_main(void)
-{
+#include "sensesp/sensors/analog_input.h"
+#include "sensesp/sensors/digital_input.h"
+#include "sensesp/sensors/sensor.h"
+#include "sensesp/signalk/signalk_output.h"
+#include "sensesp/system/lambda_consumer.h"
+#include "sensesp_app_builder.h"
 
+using namespace sensesp;
+
+reactesp::ReactESP app;
+
+// The setup function performs one-time application initialization.
+void setup() {
+#ifndef SERIAL_DEBUG_DISABLED
+  SetupSerialDebug(115200);
+#endif
+
+  // Construct the global SensESPApp() object
+  SensESPAppBuilder builder;
+  sensesp_app = (&builder)
+                    // Set a custom hostname for the app.
+                    ->set_hostname("my-sensesp-project")
+                    // Optionally, hard-code the WiFi and Signal K server
+                    // settings. This is normally not needed.
+                    //->set_wifi("My WiFi SSID", "my_wifi_password")
+                    //->set_sk_server("192.168.10.3", 80)
+                    ->get_app();
+
+
+  // Start networking, SK server connections and other SensESP internals
+  sensesp_app->start();
 }
+
+void loop() {  app.tick(); }
